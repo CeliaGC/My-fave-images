dd97414cef951c138b388bd250c30c06cccad00b
CeliaGC <you@example.comgarciacastillacelia@gmail.com>
2023-03-16 20:54:04 +0100

    api-builds-already

---
+++ mfi-back/mfi-API/Entities/ImageItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class ImageItem
+    {
+        public ImageItem()
+        {
+            IsActive = true;
+
+        }
+
+        public int Id { get; set; }
+        public string ImageName { get; set; }
+        public string ImageSource { get; set; }
+        public string Category { get; set; }
+        public Guid IdWeb { get; set; }
+        public DateTime InsertDate { get; set; }
+        public bool IsActive { get; private set; }
+
+    }
+}
